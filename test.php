9c1af320e16f6f7ac32208f6a1c5c6b166eb9f93
Sina <sina6002@gmail.com>
2014-04-03 19:48:50 -0400

    image validation

--- test.php
+++ test.php
@@ -1,6 +1,16 @@
 <?php
 
 $path="test.jpg";
+$size = getimagesize($path);
+    if(!$size) {
+        return 0;
+    }
+else{
+echo "valid image!\n";
+var_export($size);
+}
+
+
 $image = imagecreatefromjpeg($path);
 
 ?>
